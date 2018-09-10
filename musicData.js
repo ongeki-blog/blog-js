e646c604bd9604834f2d9180c31ae29905a498fe
ongeki-blog <42358859+ongeki-blog@users.noreply.github.com>
2018-09-10 13:59:28 +0900

    Create musicData.js

---
+++ musicData.js
@@ -0,0 +1,80 @@
+<script>
+//下記スクリプトのデータを丸々コピーしてサイトを運営するのはやめてください。
+var CmusicDB= [
+{
+  musicName: "Bad Apple!! feat.nomico",
+  genre:"東方Project",
+  CollaborationName:"東方Project",
+  artist: "Masayoshi Minoshima",
+  BPM:"138",
+  URL:"https://www.ongeki-blog.com/entry/2018/09/01/072631",
+  musicTime:"2:12",
+  chapter:"東方Project",
+  Vscha:"博麗霊夢(Lv.1)",
+  banMethod:"チャプター:東方Project",
+  musicDate:"2018.8.30",
+  basicArray:["2","","","","","","","","",""],
+  advancedArray:["5","","","","","","","","",""],
+  expertArray:["8","","","","","","","","","ものくろっく"],
+  masterArray:["12","870","","","","","","44","12.3","黒にするrioN"],
+  picture:'<img class="magnifiable" src="https://lh3.googleusercontent.com/-tyMsRoNTTfE/W4hmbBYFIbI/AAAAAAAAAYM/U-PNm0LzDN0XjkD1NxDWuBERcF6CswElwCE0YBhgL/s1024/1535432119.jpg" />'
+},
+{
+  musicName: "ナイト・オブ・ナイツ",
+  genre:"東方Project",
+  CollaborationName:"東方Project",
+  artist: "ビートまりお",
+  BPM:"180",
+  URL:"https://www.ongeki-blog.com/entry/2018/09/02/030633",
+  musicTime:"1:54",
+  chapter:"東方Project",
+  Vscha:"霧雨魔理沙(Lv.1)",
+  banMethod:"チャプター:東方Project",
+  musicDate:"2018.8.30",
+  basicArray:["3","223","","","","","","25","",""],
+  advancedArray:["7","359","","","","","","27","",""],
+  expertArray:["9+","567","","","","","","143","","rioN"],
+  masterArray:["12","985","","","","","","42","12.6","緑化委員長"],
+  picture:'<img class="magnifiable" src="https://lh3.googleusercontent.com/--60lsiaQVUw/W4qBfLEJlnI/AAAAAAAAAeE/Uz_SJUjpDn0XCWrRtBVaGi4g8u9Rdcr3wCE0YBhgL/s1024/Night%2Bof%2Bknights.png" itemprop="image">'
+},
+{
+  musicName: "Grip & Break down !!",
+  genre:"東方Project",
+  CollaborationName:"東方Project",
+  artist: "SOUND HOLIC feat.Nana Takahashi",
+  BPM:"160",
+  URL:"https://www.ongeki-blog.com/entry/2018/09/02/122059",
+  musicTime:"2:20",
+  chapter:"東方Project",
+  Vscha:"博麗霊夢(Lv.5)",
+  banMethod:"ジュエル15個",
+  musicDate:"2018.8.30",
+  basicArray:["3","","","","","","","","",""],
+  advancedArray:["6","","","","","","","","",""],
+  expertArray:["9","653","","","","","","125","","緑化委員長"],
+  masterArray:["11+","922","","","","","","121","11.8","緑化委員長"],
+  picture:'<img class="magnifiable" src="https://lh3.googleusercontent.com/-Z379I20XBjU/W4sccwqOb2I/AAAAAAAAAhA/Q5Qpkz1_U40HXhBCbr2NXDBUGLF7CvYCwCE0YBhgL/s1024/Grip%2B%2526%2BBreak%2Bdown%2B%2521%2521.jpg" />'
+},
+{
+  musicName: "幻想のサテライト",
+  genre:"東方Project",
+  CollaborationName:"東方Project",
+  artist: "豚乙女",
+  BPM:"230",
+  URL:"",
+  musicTime:"2:23",
+  chapter:"東方Project",
+  Vscha:"霧雨魔理沙(Lv.5)",
+  banMethod:"ジュエル30個",
+  musicDate:"2018.8.30",
+  basicArray:["3","","","","","","","","",""],
+  advancedArray:["7","","","","","","","","",""],
+  expertArray:["10","764","","","","","","75","","rioN"],
+  masterArray:["13","1,024","","","","","","94","13.0","緑化委員長"],
+  picture:'<img class="magnifiable" src="https://lh3.googleusercontent.com/-Z379I20XBjU/W4sccwqOb2I/AAAAAAAAAhA/Q5Qpkz1_U40HXhBCbr2NXDBUGLF7CvYCwCE0YBhgL/s1024/Grip%2B%2526%2BBreak%2Bdown%2B%2521%2521.jpg" />'
+}
+]
+</script>
+
+
+
