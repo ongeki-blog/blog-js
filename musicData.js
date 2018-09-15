ea36a279710876787bfb38d020e4dfcd5b1af6df
ongeki-blog <42358859+ongeki-blog@users.noreply.github.com>
2018-09-15 16:26:33 +0900

    Update musicData.js

--- musicData.js
+++ musicData.js
@@ -2,24 +2,6 @@
 //下記スクリプトのデータを丸々コピーしてサイトを運営するのはやめてください。
 var CmusicDB= [
 {
-  musicName: "Perfect Shining!!",
-  genre:"オンゲキ",
-  CollaborationName:"1-01",
-  artist: "曲：宮崎誠 歌：星咲 あかり(CV:赤尾 ひかる)",
-  BPM:"182",
-  URL:"",
-  musicTime:"2:23",
-  chapter:"チャプター01「目指せ！いっぱいのエール！」",
-  Vscha:"星咲あかり(Lv.1)",
-  banMethod:"チャプター:目指せ！いっぱいのエール！",
-  musicDate:"2018.7.26",
-  basicArray:["3","377","","","","","","47","",""],
-  advancedArray:["6","565","","","","","","52","",""],
-  expertArray:["8","605","","","","","","103","","ものくろっく"],
-  masterArray:["11","918","","","","","","133","11.5","黒にするrioN"],
-  picture:'<img class="magnifiable" src="https://lh3.googleusercontent.com/-rYziTw_sVeM/W5yUi6Q4WrI/AAAAAAAAAw4/OEQMf55VTAETYdRM2C_XGTShs2gYFhxVwCE0YBhgL/s1024/1532602274.jpg" />'
-},
-{
   musicName: "Bad Apple!! feat.nomico",
   genre:"東方Project",
   CollaborationName:"東方Project",
