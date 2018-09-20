2e0f9f4419079efcfb3f2bbd41577c08eb3b46f8
ongeki-blog <42358859+ongeki-blog@users.noreply.github.com>
2018-09-21 02:06:33 +0900

    Update ongekiApp-getD.js

--- ongekiApp-getD.js
+++ ongekiApp-getD.js
@@ -3,10 +3,15 @@ var playerDate = {
   player_level:"",
   player_trophy:"",
   player_ratingNow:"",
-  player_ratingMax:""
+  player_ratingMax:"",
+  player_battlePoint:"",
   };
 
 $(function(){
-
-alert(  $('.name_block span').html());
+  playerDate.player_name=$('.name_block span').html();
+  playerDate.player_level=$('.lv_block span').html();
+  playerDate.player_trophy=$('.trophy_block span').html();
+  playerDate.player_ratingNow=$('.rating_field span').html();
+  playerDate.player_battlePoint=$('.battle_point_10').html();
+alert(playerDate);
 });
