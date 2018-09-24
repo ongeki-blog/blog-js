6db1282855989bd7af115b85d996be2d606da55a
ongeki-blog <42358859+ongeki-blog@users.noreply.github.com>
2018-09-24 14:48:39 +0900

    Update musicDB.js

--- musicDB.js
+++ musicDB.js
@@ -16,6 +16,24 @@ var musicDB=[
   expertArray:["8","605","","","","","","103","","ものくろっく"],
   masterArray:["11","918","","","","","","133","11.5","黒にするrioN"],
   picture:'<img class="magnifiable" src="https://lh3.googleusercontent.com/-rYziTw_sVeM/W5yUi6Q4WrI/AAAAAAAAAw4/OEQMf55VTAETYdRM2C_XGTShs2gYFhxVwCE0YBhgL/s1024/1532602274.jpg" />'
+},
+{
+  musicName: "STARTLINER",
+  genre:"オンゲキ",
+  class:"1-01-01",
+  artist: "曲：宮崎誠 歌：星咲 あかり(CV:赤尾 ひかる)",
+  BPM:"182",
+  URL:"",
+  musicTime:"2:23",
+  chapter:"「目指せ！いっぱいのエール！」",
+  Vscha:"星咲あかり(Lv.1)",
+  banMethod:"チャプター:目指せ！いっぱいのエール！",
+  musicDate:"2018.7.26",
+  basicArray:["2","369","","","","","","47","",""],
+  advancedArray:["5","432","","","","","","52","",""],
+  expertArray:["8+","832","","","","","","103","","ものくろっく"],
+  masterArray:["11","819","","","","","","133","11.0","黒にするrioN"],
+  picture:'<img class="magnifiable" src="https://lh3.googleusercontent.com/-rYziTw_sVeM/W5yUi6Q4WrI/AAAAAAAAAw4/OEQMf55VTAETYdRM2C_XGTShs2gYFhxVwCE0YBhgL/s1024/1532602274.jpg" />'
 }
 
 
