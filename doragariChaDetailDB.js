8822538acaf80c08cf3be8387f2acd768dbba527
ongeki-blog <42358859+ongeki-blog@users.noreply.github.com>
2018-09-29 15:23:04 +0900

    Create doragariChaDetailDB.js

---
+++ doragariChaDetailDB.js
@@ -0,0 +1,21 @@
+ChaDetailDB=[
+{
+name:"疾風怒濤・弱体+15%",
+type:'弱体化系',
+Detail:'ヒット数15以上で、敵にデバフを与える確率が15%アップする',
+src:'<img class="magnifiable" src="https://lh3.googleusercontent.com/-_Szs-m1yWUw/W68Xeq4tFpI/AAAAAAAABXE/GdpBZGeLbTAFlEwm7JZfV_gL1DmTb63DwCE0YBhgL/s1024/IMG_1277.PNG" />'
+},
+{
+name:"睡眠耐性+50%",
+type:'耐性系',
+Detail:'「睡眠状態」になる確率が50%ダウンする',
+src:'<img class="magnifiable" src="https://lh3.googleusercontent.com/-GChOaYNmMMY/W68XemVtU_I/AAAAAAAABXE/5LWAWcmpl20SP6OjUm-kXrcnTDFI-VeqACE0YBhgL/s1024/IMG_1278.PNG" />'
+},
+{
+name:"ブレイク特攻+25%",
+type:'特攻系',
+Detail:'ブレイク中の敵へ与えるダメージが25%アップする',
+src:'<img class="magnifiable" src="https://lh3.googleusercontent.com/-9-CGLJCpgS8/W68Xeux3bqI/AAAAAAAABXE/BQ2dvI6i3yQmWZ1helFugW_xcAwmAjbNACE0YBhgL/s1024/IMG_1279.PNG" />'
+}
+
+];
