0a3802ea11afae3778a31b6dad4ba5efbb2b54bc
ongeki-blog <42358859+ongeki-blog@users.noreply.github.com>
2018-09-29 18:43:56 +0900

    Update doragariChaDB.js

--- doragariChaDB.js
+++ doragariChaDB.js
@@ -24,7 +24,8 @@ sortPicture:'<img class="magnifiable" src="https://lh3.googleusercontent.com/-Sh
 chaPicture:'<img class="magnifiable" src="https://lh3.googleusercontent.com/-iaFTJ7dHgrM/W67rkIpGvxI/AAAAAAAABWM/w0TNXp--uyUTYVY0emrh3dvjaAfwm-sugCE0YBhgL/s1024/IMG_1275.PNG" />',
 DchaPicture:'<img class="magnifiable" src="https://lh3.googleusercontent.com/-UHCZuMecKrw/W67rkI0zoVI/AAAAAAAABWM/nbDvPd8o2rY3qTkUSoG3rf0R_OIGnvsFACE0YBhgL/s1024/IMG_1276.PNG" />',
 skil1Picture:'<img class="magnifiable" src="https://lh3.googleusercontent.com/-C6c8XaCReCM/W67sMh-ALSI/AAAAAAAABWg/k9VTrqoVlSkC3UoU84VTrugBjDN1JrhPwCE0YBhgL/s1024/IMG_1270.PNG" width="96"/>',
-skil2Picture:'<img class="magnifiable" src="https://lh3.googleusercontent.com/-Cs9v8iWPSsA/W67sMn18paI/AAAAAAAABWg/-vOtrxsvsIEL2_JzgzB_1moKvvwnddo0QCE0YBhgL/s1024/IMG_1271.PNG" width="96"/>'
+skil2Picture:'<img class="magnifiable" src="https://lh3.googleusercontent.com/-Cs9v8iWPSsA/W67sMn18paI/AAAAAAAABWg/-vOtrxsvsIEL2_JzgzB_1moKvvwnddo0QCE0YBhgL/s1024/IMG_1271.PNG" width="96"/>',
+URL:''
 },
 {
 Name:"エゼリット",
@@ -51,6 +52,7 @@ sortPicture:'<img class="magnifiable" src="https://lh3.googleusercontent.com/-Sh
 chaPicture:'<img class="magnifiable" src="https://lh3.googleusercontent.com/-iaFTJ7dHgrM/W67rkIpGvxI/AAAAAAAABWM/w0TNXp--uyUTYVY0emrh3dvjaAfwm-sugCE0YBhgL/s1024/IMG_1275.PNG" />',
 DchaPicture:'<img class="magnifiable" src="https://lh3.googleusercontent.com/-UHCZuMecKrw/W67rkI0zoVI/AAAAAAAABWM/nbDvPd8o2rY3qTkUSoG3rf0R_OIGnvsFACE0YBhgL/s1024/IMG_1276.PNG" />',
 skil1Picture:'<img class="magnifiable" src="https://lh3.googleusercontent.com/-C6c8XaCReCM/W67sMh-ALSI/AAAAAAAABWg/k9VTrqoVlSkC3UoU84VTrugBjDN1JrhPwCE0YBhgL/s1024/IMG_1270.PNG" width="96"/>',
-skil2Picture:'<img class="magnifiable" src="https://lh3.googleusercontent.com/-Cs9v8iWPSsA/W67sMn18paI/AAAAAAAABWg/-vOtrxsvsIEL2_JzgzB_1moKvvwnddo0QCE0YBhgL/s1024/IMG_1271.PNG" width="96"/>'
+skil2Picture:'<img class="magnifiable" src="https://lh3.googleusercontent.com/-Cs9v8iWPSsA/W67sMn18paI/AAAAAAAABWg/-vOtrxsvsIEL2_JzgzB_1moKvvwnddo0QCE0YBhgL/s1024/IMG_1271.PNG" width="96"/>',
+URL:'https://www.dragaliguide.com/entry/2018/09/29/【ドラガリアロスト】エゼリットの評価とアビリ'
 }
 ];
