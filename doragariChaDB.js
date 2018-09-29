b119b6f7389909329a81bfb6c88f95d97aaf56da
ongeki-blog <42358859+ongeki-blog@users.noreply.github.com>
2018-09-29 12:08:50 +0900

    Create doragariChaDB.js

---
+++ doragariChaDB.js
@@ -0,0 +1,40 @@
+ChaDB=[
+{
+Name:"ミコト",
+Name2:"ブリリアントジーニアス 光輝の天才",
+attribute:"火",
+rare:"5",
+date:"2018.9.27",
+type:"補助",
+ste:[,],
+job:"短剣",
+skil1:"ランブリングミーティア",
+skil1Detail:"前方の敵に火属性のダメージを与える。攻撃時、移動操作が可能",
+skil2:"ブリリアントインフェルノ",
+skil2Detail:"15秒間【インフェルノモード】を発動。【インフェルノモード】中は自身の攻撃力が10%アップし、通常攻撃に防御力をダウンさせる効果が付与される。",
+exAbi:"クリティカル率+5%",
+exAbiDetail:"",
+Abi:["","",""]
+},
+{
+Name:"エゼリット",
+Name2:"ブリリアントジーニアス 光輝の天才",
+attribute:"火",
+rare:"5",
+date:"2018.9.27",
+type:"補助",
+ste:[,],
+job:"短剣",
+skil1:"ランブリングミーティア",
+skil1Detail:"前方の敵に火属性のダメージを与える。攻撃時、移動操作が可能",
+skil2:"ブリリアントインフェルノ",
+skil2Detail:"15秒間【インフェルノモード】を発動。【インフェルノモード】中は自身の攻撃力が10%アップし、通常攻撃に防御力をダウンさせる効果が付与される。",
+exAbi:"クリティカル率+5%",
+exAbiDetail:"パーティ全員のクリティカル確率が5%アップする。",
+Abi:["疾風怒濤・弱体+15%","睡眠耐性+50%","ブレイク特攻+25%"],
+chaPicture:'<img class="magnifiable" src="https://lh3.googleusercontent.com/-iaFTJ7dHgrM/W67rkIpGvxI/AAAAAAAABWM/w0TNXp--uyUTYVY0emrh3dvjaAfwm-sugCE0YBhgL/s1024/IMG_1275.PNG" />',
+3DchaPicture:'<img class="magnifiable" src="https://lh3.googleusercontent.com/-UHCZuMecKrw/W67rkI0zoVI/AAAAAAAABWM/nbDvPd8o2rY3qTkUSoG3rf0R_OIGnvsFACE0YBhgL/s1024/IMG_1276.PNG" />',
+skil1Picture:'<img class="magnifiable" src="https://lh3.googleusercontent.com/-C6c8XaCReCM/W67sMh-ALSI/AAAAAAAABWg/k9VTrqoVlSkC3UoU84VTrugBjDN1JrhPwCE0YBhgL/s1024/IMG_1270.PNG" />',
+skil2Picture:'<img class="magnifiable" src="https://lh3.googleusercontent.com/-Cs9v8iWPSsA/W67sMn18paI/AAAAAAAABWg/-vOtrxsvsIEL2_JzgzB_1moKvvwnddo0QCE0YBhgL/s1024/IMG_1271.PNG" />'
+}
+];
