086711a974d2ddf1ac7be08cc02c26274ea9b23d
ongeki-blog <42358859+ongeki-blog@users.noreply.github.com>
2018-09-29 18:23:12 +0900

    Update doragariChaDB.js

--- doragariChaDB.js
+++ doragariChaDB.js
@@ -1,20 +1,31 @@
 ChaDB=[
 {
+{
 Name:"ミコト",
 Name2:"ブリリアントジーニアス 光輝の天才",
 attribute:"火",
 rare:"5",
 date:"2018.9.27",
 type:"補助",
-ste:[,],
+ste:["sen","HP","kougei"],
 job:"短剣",
+Rank:"S",
+reRank:"S",
 skil1:"ランブリングミーティア",
 skil1Detail:"前方の敵に火属性のダメージを与える。攻撃時、移動操作が可能",
 skil2:"ブリリアントインフェルノ",
 skil2Detail:"15秒間【インフェルノモード】を発動。【インフェルノモード】中は自身の攻撃力が10%アップし、通常攻撃に防御力をダウンさせる効果が付与される。",
 exAbi:"クリティカル率+5%",
-exAbiDetail:"",
-Abi:["","",""]
+exAbiDetail:"パーティ全員のクリティカル確率が5%アップする。",
+exSrc='<img class="magnifiable" src="https://lh3.googleusercontent.com/-KOKBLmiyX9c/W682sbE7iII/AAAAAAAABXo/cJQNOnRryeoOxuFG-nzNLsjr7xpiGfG8QCE0YBhgL/s1024/IMG_1284.PNG" width="96" />',
+Abi:["疾風怒濤・弱体+15%","睡眠耐性+50%","ブレイク特攻+25%"],
+AbiSrc:['<img class="magnifiable" src="https://lh3.googleusercontent.com/-_Szs-m1yWUw/W68Xeq4tFpI/AAAAAAAABXE/GdpBZGeLbTAFlEwm7JZfV_gL1DmTb63DwCE0YBhgL/s1024/IMG_1277.PNG" width="96"/>','<img class="magnifiable" src="https://lh3.googleusercontent.com/-GChOaYNmMMY/W68XemVtU_I/AAAAAAAABXE/5LWAWcmpl20SP6OjUm-kXrcnTDFI-VeqACE0YBhgL/s1024/IMG_1278.PNG" width="96"/>','<img class="magnifiable" src="https://lh3.googleusercontent.com/-9-CGLJCpgS8/W68Xeux3bqI/AAAAAAAABXE/BQ2dvI6i3yQmWZ1helFugW_xcAwmAjbNACE0YBhgL/s1024/IMG_1279.PNG" width="96"/>'],
+AbiD:['ヒット数15以上で、敵にデバフを与える確率が15%アップする','「睡眠状態」になる確率が50%ダウンする','ブレイク中の敵へ与えるダメージが25%アップする'],
+sortPicture:'',
+chaPicture:'<img class="magnifiable" src="https://lh3.googleusercontent.com/-iaFTJ7dHgrM/W67rkIpGvxI/AAAAAAAABWM/w0TNXp--uyUTYVY0emrh3dvjaAfwm-sugCE0YBhgL/s1024/IMG_1275.PNG" />',
+DchaPicture:'<img class="magnifiable" src="https://lh3.googleusercontent.com/-UHCZuMecKrw/W67rkI0zoVI/AAAAAAAABWM/nbDvPd8o2rY3qTkUSoG3rf0R_OIGnvsFACE0YBhgL/s1024/IMG_1276.PNG" />',
+skil1Picture:'<img class="magnifiable" src="https://lh3.googleusercontent.com/-C6c8XaCReCM/W67sMh-ALSI/AAAAAAAABWg/k9VTrqoVlSkC3UoU84VTrugBjDN1JrhPwCE0YBhgL/s1024/IMG_1270.PNG" width="96"/>',
+skil2Picture:'<img class="magnifiable" src="https://lh3.googleusercontent.com/-Cs9v8iWPSsA/W67sMn18paI/AAAAAAAABWg/-vOtrxsvsIEL2_JzgzB_1moKvvwnddo0QCE0YBhgL/s1024/IMG_1271.PNG" width="96"/>'
 },
 {
 Name:"エゼリット",
@@ -37,6 +48,7 @@ exSrc='<img class="magnifiable" src="https://lh3.googleusercontent.com/-KOKBLmiy
 Abi:["疾風怒濤・弱体+15%","睡眠耐性+50%","ブレイク特攻+25%"],
 AbiSrc:['<img class="magnifiable" src="https://lh3.googleusercontent.com/-_Szs-m1yWUw/W68Xeq4tFpI/AAAAAAAABXE/GdpBZGeLbTAFlEwm7JZfV_gL1DmTb63DwCE0YBhgL/s1024/IMG_1277.PNG" width="96"/>','<img class="magnifiable" src="https://lh3.googleusercontent.com/-GChOaYNmMMY/W68XemVtU_I/AAAAAAAABXE/5LWAWcmpl20SP6OjUm-kXrcnTDFI-VeqACE0YBhgL/s1024/IMG_1278.PNG" width="96"/>','<img class="magnifiable" src="https://lh3.googleusercontent.com/-9-CGLJCpgS8/W68Xeux3bqI/AAAAAAAABXE/BQ2dvI6i3yQmWZ1helFugW_xcAwmAjbNACE0YBhgL/s1024/IMG_1279.PNG" width="96"/>'],
 AbiD:['ヒット数15以上で、敵にデバフを与える確率が15%アップする','「睡眠状態」になる確率が50%ダウンする','ブレイク中の敵へ与えるダメージが25%アップする'],
+sortPicture:'<img class="magnifiable" src="https://lh3.googleusercontent.com/-ShaTpwXwzJI/W69EPROLgoI/AAAAAAAABY0/dLDvGekgItkCcjCbvvaobB4diRZyv9I2wCE0YBhgL/s1024/IMG_1285.PNG" width="64"/>',
 chaPicture:'<img class="magnifiable" src="https://lh3.googleusercontent.com/-iaFTJ7dHgrM/W67rkIpGvxI/AAAAAAAABWM/w0TNXp--uyUTYVY0emrh3dvjaAfwm-sugCE0YBhgL/s1024/IMG_1275.PNG" />',
 DchaPicture:'<img class="magnifiable" src="https://lh3.googleusercontent.com/-UHCZuMecKrw/W67rkI0zoVI/AAAAAAAABWM/nbDvPd8o2rY3qTkUSoG3rf0R_OIGnvsFACE0YBhgL/s1024/IMG_1276.PNG" />',
 skil1Picture:'<img class="magnifiable" src="https://lh3.googleusercontent.com/-C6c8XaCReCM/W67sMh-ALSI/AAAAAAAABWg/k9VTrqoVlSkC3UoU84VTrugBjDN1JrhPwCE0YBhgL/s1024/IMG_1270.PNG" width="96"/>',
