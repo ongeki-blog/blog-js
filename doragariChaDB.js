6cafb0ac74dd772db54907ca7b332960af8d3a4b
ongeki-blog <42358859+ongeki-blog@users.noreply.github.com>
2018-09-29 15:02:20 +0900

    Update doragariChaDB.js

--- doragariChaDB.js
+++ doragariChaDB.js
@@ -36,7 +36,7 @@ Rank:"S",
 reRank:"S",
 chaPicture:'<img class="magnifiable" src="https://lh3.googleusercontent.com/-iaFTJ7dHgrM/W67rkIpGvxI/AAAAAAAABWM/w0TNXp--uyUTYVY0emrh3dvjaAfwm-sugCE0YBhgL/s1024/IMG_1275.PNG" />',
 DchaPicture:'<img class="magnifiable" src="https://lh3.googleusercontent.com/-UHCZuMecKrw/W67rkI0zoVI/AAAAAAAABWM/nbDvPd8o2rY3qTkUSoG3rf0R_OIGnvsFACE0YBhgL/s1024/IMG_1276.PNG" />',
-skil1Picture:'<img class="magnifiable" src="https://lh3.googleusercontent.com/-C6c8XaCReCM/W67sMh-ALSI/AAAAAAAABWg/k9VTrqoVlSkC3UoU84VTrugBjDN1JrhPwCE0YBhgL/s1024/IMG_1270.PNG" />',
-skil2Picture:'<img class="magnifiable" src="https://lh3.googleusercontent.com/-Cs9v8iWPSsA/W67sMn18paI/AAAAAAAABWg/-vOtrxsvsIEL2_JzgzB_1moKvvwnddo0QCE0YBhgL/s1024/IMG_1271.PNG" />'
+skil1Picture:'<img class="magnifiable" src="https://lh3.googleusercontent.com/-C6c8XaCReCM/W67sMh-ALSI/AAAAAAAABWg/k9VTrqoVlSkC3UoU84VTrugBjDN1JrhPwCE0YBhgL/s1024/IMG_1270.PNG" width="96"/>',
+skil2Picture:'<img class="magnifiable" src="https://lh3.googleusercontent.com/-Cs9v8iWPSsA/W67sMn18paI/AAAAAAAABWg/-vOtrxsvsIEL2_JzgzB_1moKvvwnddo0QCE0YBhgL/s1024/IMG_1271.PNG" width="96"/>'
 }
 ];
