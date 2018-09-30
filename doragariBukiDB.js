17f49ccca5395c93a1d6439456c3f27151eca63a
ongeki-blog <42358859+ongeki-blog@users.noreply.github.com>
2018-10-01 07:39:13 +0900

    Create doragariBukiDB.js

---
+++ doragariBukiDB.js
@@ -0,0 +1,27 @@
+ChaDB=[
+{
+Name:"ミコト",
+get:"可",
+maxLV:"",
+attribute:"火",
+date:"2018.9.27",
+ste:["sen","HP","kougei","MAXS","MAXH","MAXK"],
+Rank:"SS",
+skil1:"熾炎剣・魔障覆滅",
+skil1Detail:"前方の敵に火属性のダメージを与える。敵にヒットさせると【構え】が発動。【構え】中は、攻撃力がアップし、スキルが強化される。【構え】は2段階目にスキルをヒットさせるか、時間経過で解除。",
+skil2:"熾炎剣・胸中燈火",
+skil2Detail:"10秒間、自身の攻撃速度を15%アップする。",
+exAbi:"攻撃力+5%",
+exAbiDetail:"パーティ全員の攻撃力が5%アップする。",
+exSrc:'<img class="magnifiable" src="https://lh3.googleusercontent.com/-_Szs-m1yWUw/W68Xeq4tFpI/AAAAAAAABXE/GdpBZGeLbTAFlEwm7JZfV_gL1DmTb63DwCE0YBhgL/s1024/IMG_1277.PNG" width="96"/>',
+Abi:["HP70%で会心+8%","気絶耐性+50%","クリティカル率+6%"],
+AbiSrc:['<img class="magnifiable" src="https://lh3.googleusercontent.com/-KOKBLmiyX9c/W682sbE7iII/AAAAAAAABXo/cJQNOnRryeoOxuFG-nzNLsjr7xpiGfG8QCE0YBhgL/s1024/IMG_1284.PNG" width="96" />','<img class="magnifiable" src="https://lh3.googleusercontent.com/-TYV6_aL0w08/W69QvnFuONI/AAAAAAAABZ0/ajtCjNp8VLcvV0tsjU48gBArpWVqHdCDQCE0YBhgL/s1024/IMG_1289.PNG" width="96"/>','<img class="magnifiable" src="https://lh3.googleusercontent.com/-KOKBLmiyX9c/W682sbE7iII/AAAAAAAABXo/cJQNOnRryeoOxuFG-nzNLsjr7xpiGfG8QCE0YBhgL/s1024/IMG_1284.PNG" width="96" />'],
+AbiD:['HP70％以上で攻撃がクリティカルになる確率が8％アップする。','「気絶」状態になる確率が50％ダウンする。','攻撃がクリティカルになる確率が6％アップする。'],
+sortPicture:'<img class="magnifiable" src="https://lh3.googleusercontent.com/-7RnDRY0UXfE/W69Q45diwSI/AAAAAAAABZ0/W3R5c_jfOHEi84ZKb1hDM65VOtfMTW7MwCE0YBhgL/s1024/IMG_1290.PNG" width="64"/>',
+chaPicture:'<img class="magnifiable" src="https://lh3.googleusercontent.com/-3R8uKG_-VXU/W69Qzq-XDGI/AAAAAAAABZ0/Swa2ocX4hiYVLC8CTh8nGooU9DkHFj_LQCE0YBhgL/s1024/IMG_1286.PNG" />',
+DchaPicture:'<img class="magnifiable" src="https://lh3.googleusercontent.com/-JEaH1oKCmuw/W69QzglPfhI/AAAAAAAABZ0/_puTG_eb5IIlSaQ0hwm7NlkXwFTAlWvAwCE0YBhgL/s1024/IMG_1287.PNG" />',
+skil1Picture:'<img class="magnifiable" src="https://lh3.googleusercontent.com/-5_PPXu9PfyQ/W69QvrnfEII/AAAAAAAABZ0/4Ev3jYPrC7sPEqzWS-GObooHCf18WAhpgCE0YBhgL/s1024/IMG_1288.PNG" width="96"/>',
+skil2Picture:'<img class="magnifiable" src="https://lh3.googleusercontent.com/-Cs9v8iWPSsA/W67sMn18paI/AAAAAAAABWg/-vOtrxsvsIEL2_JzgzB_1moKvvwnddo0QCE0YBhgL/s1024/IMG_1271.PNG" width="96"/>',
+URL:'https://www.dragaliguide.com/entry/2018/09/29/【ドラガリアロスト】ミコトの評価とアビリティ'
+}
+];
