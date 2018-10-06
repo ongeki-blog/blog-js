afdd89eb17227f53f7d103d94d55b7b33cab75c1
ongeki-blog <42358859+ongeki-blog@users.noreply.github.com>
2018-10-07 03:03:55 +0900

    Create ongeki-getmusicData.js

---
+++ ongeki-getmusicData.js
@@ -0,0 +1,24 @@
+
+
+var simplicityArray=[];
+//getMusic push
+function get_musicData(URL) {
+  var simplicityArray=[];
+    $.ajax({
+        type: 'GET',
+        url: URL,
+        dataType: 'html',
+        success: function(data) {
+          $(data).find('form').each(function(ippon,form){
+            $(form).find('a').each(function(){
+              simplicityArray.push($(this).attr('href'));
+            });
+          });
+         }
+          console.log(simplicityArray);
+    });
+}
+$(function(){
+get_musicData("http://ongeki-club.bingoworlds.com/music/all");
+});
+window.confirm('スマートフォン向けサイトに移動しますか？')
