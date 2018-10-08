cfda11935a72e6b11136c29c71c22c22984f6d2e
ongeki-blog <42358859+ongeki-blog@users.noreply.github.com>
2018-10-09 00:54:21 +0900

    Update ongeki-getmusicData.js

--- ongeki-getmusicData.js
+++ ongeki-getmusicData.js
@@ -1,9 +1,14 @@
 
 var simplicityArray=[];
+var simplicityArrayq=[];
 $(function(){
   $('a').each(function(ippon,form){
     simplicityArray.push($(this).attr('href'));
   });
+  $('td').each(function(ippon,form){
+    simplicityArrayq.push($(this).text());
+  });
   console.log(simplicityArray);
+  console.log(simplicityArrayq);
 });
 
