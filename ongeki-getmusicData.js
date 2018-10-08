ca78e6a318a441c543cb4cdd84c7b476bb3580b8
ongeki-blog <42358859+ongeki-blog@users.noreply.github.com>
2018-10-09 01:23:38 +0900

    Update ongeki-getmusicData.js

--- ongeki-getmusicData.js
+++ ongeki-getmusicData.js
@@ -3,12 +3,25 @@ var simplicityArray=[];
 var simplicityArrayq=[];
 $(function(){
   $('th').each(function(ippon,form){
-    simplicityArray.push($(this).text().replace(/↵/g, ''));
+    simplicityArray.push($(this).text().replace(/¥n/g, ''));
   });
   $('td').each(function(ippon,form){
-    simplicityArrayq.push($(this).text().replace(/↵/g, ''));
+    simplicityArrayq.push($(this).text().replace(/\u002f/g, '.'));
   });
   console.log(simplicityArray);
   console.log(simplicityArrayq);
+  alert(",{musicName: '"+$('h2').text+"',BPM:'',musicTime:'',class:'',chapter:'「」',URL:'',Vscha:'(Lv.1)',banMethod:'チャプター:',picture:'',musicDate:'"+simplicityArrayq[1]+"',genre:'"+simplicityArrayq[3]"',artist: '"+simplicityArrayq[5]+"',basicArray:['"+simplicityArray[9]+"','','','','','','','','',''],advancedArray:['"+simplicityArray[10]+"','','','','','','','','',''],expertArray:['"+simplicityArray[11]+"','"+simplicityArray[23]+"','','','','','','"+simplicityArray[35]+"','',''],masterArray:['"+simplicityArray[12]+"','"+simplicityArray[48]+"','','','','','','"+simplicityArray[60]+"','"+simplicityArrayq[4]+"',''],"});
 });
 
+
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
