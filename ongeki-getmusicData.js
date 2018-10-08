63b66ce136cd6fb1ee84e76107f0b1bc63548866
ongeki-blog <42358859+ongeki-blog@users.noreply.github.com>
2018-10-09 00:23:29 +0900

    Update ongeki-getmusicData.js

--- ongeki-getmusicData.js
+++ ongeki-getmusicData.js
@@ -1,5 +1,9 @@
 
-
+document.body.appendChild((function(){
+  var jq = document.createElement("script");
+  jq.src = '//ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js';
+  return jq;
+})());
 var simplicityArray=[];
 //getMusic push
 function get_musicData(URL) {
